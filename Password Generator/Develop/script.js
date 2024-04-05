afce83bc57f0ac027dfb2c8b0a61986eb89abc7a
jmartincampos <jmartincampos18@gmail.com>
2024-04-04 19:58:54 -0700

    NEW UPDATES

--- Password Generator/Develop/script.js
+++ Password Generator/Develop/script.js
@@ -27,55 +27,40 @@ function generatePassword(length, includeLowercase, includeUppercase, includeNum
   return password;
 }
 
-const passwordLength = 12;
-const includeLowercase = true;
-const includeUppercase = true;
-const includeNumeric = true;
-const includeSpecial = true;
-
-const password = generatePassword (
-                    length,
-                    includeLowercase,
-                    includeUppercase,
-                    includeNumeric,
-                    includeSpecial,
-                  
-);
-
- 
 // Get references to the #generate element
-//var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
-//function writePassword() {
-//  var password = generatePassword();
- // var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  //passwordText.value = password;
+  passwordText.value = password;
 
-//}
+}
 
  //Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
 
 // Function to generate password base on user based criteria
-//function generatePassword() {
+function generatePassword() {
   // Array types
-  //var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-  //var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  //var numericChars = "0123456789";
-  // var specialChars = "~!@#$%^&*()_+{}[]\|';:,.<>;"
-//}
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "~!@#$%^&*()_+{}[]\|';:,.<>;"
+}
 
 // Varibales to store user choices
-//var passwordLength = prompt ("Must be 8 to 12 characters):")
+var passwordLength = prompt ("Must be 8 to 12 characters):")
 
-//if (passwordLength < 8 || passwordLength > 12 || isNaN(passwordLength)) {
-  //alert("Please enter a valid password length between 8 and 128 characters.");
-  //return "" // signify error
-//}
+if (passwordLength < 8 || passwordLength > 12 || isNaN(passwordLength)) {
+  alert("Please enter a valid password length between 8 and 128 characters.");
+  return error;
+}
 
-//var includeLowercase = confirm("Include lowercase characters?");
-//var includeUppercase = confirm("Include uppercase characters?");
-//var includeNumeric = confirm("Include numeric characters?");
-//var includeSpecial = confirm("Include special characters?");//
+var includeLowercase = confirm("Include lowercase characters?");
+var includeUppercase = confirm("Include uppercase characters?");
+var includeNumeric = confirm("Include numeric characters?");
+var includeSpecial = confirm("Include special characters?");//
